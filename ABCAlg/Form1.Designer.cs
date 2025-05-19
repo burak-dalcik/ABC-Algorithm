cfca48149a4c485c9b13b41bf10f665453f60c56
burak <burak@DESKTOP-02M7DF2>
2025-05-19 20:45:15 +0300

    .

--- ABCAlg/Form1.Designer.cs
+++ ABCAlg/Form1.Designer.cs
@@ -28,21 +28,259 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelColony = new System.Windows.Forms.Label();
+            this.labelIter = new System.Windows.Forms.Label();
+            this.labelLimit = new System.Windows.Forms.Label();
+            this.labelDim = new System.Windows.Forms.Label();
+            this.labelFunc = new System.Windows.Forms.Label();
+            this.numericColony = new System.Windows.Forms.NumericUpDown();
+            this.numericIter = new System.Windows.Forms.NumericUpDown();
+            this.numericLimit = new System.Windows.Forms.NumericUpDown();
+            this.numericDim = new System.Windows.Forms.NumericUpDown();
+            this.comboFunc = new System.Windows.Forms.ComboBox();
+            this.buttonSolve = new System.Windows.Forms.Button();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.chartConvergence = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.numericColony)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericIter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericLimit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericDim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartConvergence)).BeginInit();
             this.SuspendLayout();
+            // 
+            // labelColony
+            // 
+            this.labelColony.AutoSize = true;
+            this.labelColony.Location = new System.Drawing.Point(173, 27);
+            this.labelColony.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelColony.Name = "labelColony";
+            this.labelColony.Size = new System.Drawing.Size(88, 16);
+            this.labelColony.TabIndex = 0;
+            this.labelColony.Text = "Kolon Boyutu:";
+            // 
+            // labelIter
+            // 
+            this.labelIter.AutoSize = true;
+            this.labelIter.Location = new System.Drawing.Point(173, 64);
+            this.labelIter.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelIter.Name = "labelIter";
+            this.labelIter.Size = new System.Drawing.Size(133, 16);
+            this.labelIter.TabIndex = 1;
+            this.labelIter.Text = "Maksimum İterasyon:";
+            // 
+            // labelLimit
+            // 
+            this.labelLimit.AutoSize = true;
+            this.labelLimit.Location = new System.Drawing.Point(173, 101);
+            this.labelLimit.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelLimit.Name = "labelLimit";
+            this.labelLimit.Size = new System.Drawing.Size(37, 16);
+            this.labelLimit.TabIndex = 2;
+            this.labelLimit.Text = "Limit:";
+            // 
+            // labelDim
+            // 
+            this.labelDim.AutoSize = true;
+            this.labelDim.Location = new System.Drawing.Point(173, 138);
+            this.labelDim.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelDim.Name = "labelDim";
+            this.labelDim.Size = new System.Drawing.Size(44, 16);
+            this.labelDim.TabIndex = 3;
+            this.labelDim.Text = "Boyut:";
+            // 
+            // labelFunc
+            // 
+            this.labelFunc.AutoSize = true;
+            this.labelFunc.Location = new System.Drawing.Point(240, 176);
+            this.labelFunc.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelFunc.Name = "labelFunc";
+            this.labelFunc.Size = new System.Drawing.Size(109, 16);
+            this.labelFunc.TabIndex = 4;
+            this.labelFunc.Text = "Test Fonksiyonu:";
+            // 
+            // numericColony
+            // 
+            this.numericColony.Location = new System.Drawing.Point(27, 21);
+            this.numericColony.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericColony.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericColony.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericColony.Name = "numericColony";
+            this.numericColony.Size = new System.Drawing.Size(133, 22);
+            this.numericColony.TabIndex = 5;
+            this.numericColony.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // numericIter
+            // 
+            this.numericIter.Location = new System.Drawing.Point(27, 62);
+            this.numericIter.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericIter.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericIter.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericIter.Name = "numericIter";
+            this.numericIter.Size = new System.Drawing.Size(133, 22);
+            this.numericIter.TabIndex = 6;
+            this.numericIter.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // numericLimit
+            // 
+            this.numericLimit.Location = new System.Drawing.Point(27, 98);
+            this.numericLimit.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericLimit.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericLimit.Minimum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericLimit.Name = "numericLimit";
+            this.numericLimit.Size = new System.Drawing.Size(133, 22);
+            this.numericLimit.TabIndex = 7;
+            this.numericLimit.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // numericDim
+            // 
+            this.numericDim.Location = new System.Drawing.Point(27, 135);
+            this.numericDim.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.numericDim.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericDim.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericDim.Name = "numericDim";
+            this.numericDim.Size = new System.Drawing.Size(133, 22);
+            this.numericDim.TabIndex = 8;
+            this.numericDim.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // comboFunc
+            // 
+            this.comboFunc.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboFunc.FormattingEnabled = true;
+            this.comboFunc.Items.AddRange(new object[] {
+            "Sphere",
+            "Rosenbrock",
+            "Rastrigin"});
+            this.comboFunc.Location = new System.Drawing.Point(27, 172);
+            this.comboFunc.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboFunc.Name = "comboFunc";
+            this.comboFunc.Size = new System.Drawing.Size(199, 24);
+            this.comboFunc.TabIndex = 5;
+            // 
+            // buttonSolve
+            // 
+            this.buttonSolve.Location = new System.Drawing.Point(27, 209);
+            this.buttonSolve.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonSolve.Name = "buttonSolve";
+            this.buttonSolve.Size = new System.Drawing.Size(133, 28);
+            this.buttonSolve.TabIndex = 6;
+            this.buttonSolve.Text = "Çöz";
+            this.buttonSolve.UseVisualStyleBackColor = true;
+            // 
+            // textResult
+            // 
+            this.textResult.Font = new System.Drawing.Font("Consolas", 10F);
+            this.textResult.Location = new System.Drawing.Point(13, 258);
+            this.textResult.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textResult.Multiline = true;
+            this.textResult.Name = "textResult";
+            this.textResult.ReadOnly = true;
+            this.textResult.Size = new System.Drawing.Size(399, 491);
+            this.textResult.TabIndex = 7;
+            // 
+            // chartConvergence
+            // 
+            this.chartConvergence.Location = new System.Drawing.Point(437, 11);
+            this.chartConvergence.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chartConvergence.Name = "chartConvergence";
+            this.chartConvergence.Size = new System.Drawing.Size(688, 738);
+            this.chartConvergence.TabIndex = 8;
+            this.chartConvergence.Text = "chart1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1333, 862);
+            this.Controls.Add(this.labelColony);
+            this.Controls.Add(this.labelIter);
+            this.Controls.Add(this.labelLimit);
+            this.Controls.Add(this.labelDim);
+            this.Controls.Add(this.labelFunc);
+            this.Controls.Add(this.numericColony);
+            this.Controls.Add(this.numericIter);
+            this.Controls.Add(this.numericLimit);
+            this.Controls.Add(this.numericDim);
+            this.Controls.Add(this.comboFunc);
+            this.Controls.Add(this.buttonSolve);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.chartConvergence);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Text = "ABC Algoritması";
+            ((System.ComponentModel.ISupportInitialize)(this.numericColony)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericIter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericLimit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericDim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartConvergence)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelColony;
+        private System.Windows.Forms.Label labelIter;
+        private System.Windows.Forms.Label labelLimit;
+        private System.Windows.Forms.Label labelDim;
+        private System.Windows.Forms.Label labelFunc;
+        private System.Windows.Forms.NumericUpDown numericColony;
+        private System.Windows.Forms.NumericUpDown numericIter;
+        private System.Windows.Forms.NumericUpDown numericLimit;
+        private System.Windows.Forms.NumericUpDown numericDim;
+        private System.Windows.Forms.ComboBox comboFunc;
+        private System.Windows.Forms.Button buttonSolve;
+        private System.Windows.Forms.TextBox textResult;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartConvergence;
     }
 }
 
--- ABCAlg/Form1.cs
+++ ABCAlg/Form1.cs
@@ -8,153 +8,24 @@ namespace ABCAlg
     public partial class Form1 : Form
     {
         private ABCAlgorithm _abc;
-        private Chart _convergenceChart;
-        private TextBox _resultTextBox;
-        private NumericUpDown _colonySizeNumeric;
-        private NumericUpDown _maxIterationsNumeric;
-        private NumericUpDown _limitNumeric;
-        private NumericUpDown _dimensionNumeric;
-        private ComboBox _functionComboBox;
-        private Button _solveButton;
 
         public Form1()
         {
             InitializeComponent();
-            InitializeCustomComponents();
-        }
-
-        private void InitializeCustomComponents()
-        {
-            // Form boyutunu ayarla
-            this.Size = new Size(1000, 700);
-            this.Text = "ABC Algoritması";
-
-            // Kontrolleri oluştur
-            _colonySizeNumeric = new NumericUpDown
-            {
-                Location = new Point(20, 20),
-                Minimum = 10,
-                Maximum = 1000,
-                Value = 50,
-                Width = 100
-            };
-
-            _maxIterationsNumeric = new NumericUpDown
-            {
-                Location = new Point(20, 50),
-                Minimum = 100,
-                Maximum = 10000,
-                Value = 1000,
-                Width = 100
-            };
-
-            _limitNumeric = new NumericUpDown
-            {
-                Location = new Point(20, 80),
-                Minimum = 10,
-                Maximum = 1000,
-                Value = 100,
-                Width = 100
-            };
-
-            _dimensionNumeric = new NumericUpDown
-            {
-                Location = new Point(20, 110),
-                Minimum = 2,
-                Maximum = 30,
-                Value = 2,
-                Width = 100
-            };
-
-            _functionComboBox = new ComboBox
-            {
-                Location = new Point(20, 140),
-                Width = 150,
-                DropDownStyle = ComboBoxStyle.DropDownList
-            };
-            _functionComboBox.Items.AddRange(new string[] { "Sphere", "Rosenbrock", "Rastrigin" });
-            _functionComboBox.SelectedIndex = 0;
-
-            _solveButton = new Button
-            {
-                Location = new Point(20, 170),
-                Text = "Çöz",
-                Width = 100
-            };
-            _solveButton.Click += SolveButton_Click;
-
-            _resultTextBox = new TextBox
-            {
-                Location = new Point(20, 210),
-                Multiline = true,
-                Width = 300,
-                Height = 400,
-                ReadOnly = true,
-                Font = new Font("Consolas", 10)
-            };
-
-            _convergenceChart = new Chart
-            {
-                Location = new Point(340, 20),
-                Size = new Size(600, 600)
-            };
-
-            var chartArea = new ChartArea();
-            chartArea.AxisX.Title = "İterasyon";
-            chartArea.AxisY.Title = "Amaç Fonksiyonu Değeri";
-            chartArea.AxisY.IsLogarithmic = true;
-            chartArea.AxisY.LogarithmBase = 10;
-            
-            var series = new Series();
-            series.ChartType = SeriesChartType.Line;
-            series.Color = Color.Blue;
-            series.BorderWidth = 2;
-            
-            _convergenceChart.ChartAreas.Add(chartArea);
-            _convergenceChart.Series.Add(series);
-
-            // Kontrolleri forma ekle
-            this.Controls.AddRange(new Control[] {
-                _colonySizeNumeric,
-                _maxIterationsNumeric,
-                _limitNumeric,
-                _dimensionNumeric,
-                _functionComboBox,
-                _solveButton,
-                _resultTextBox,
-                _convergenceChart
-            });
-
-            // Etiketleri ekle
-            AddLabel("Kolon Boyutu:", 130, 22);
-            AddLabel("Maksimum İterasyon:", 130, 52);
-            AddLabel("Limit:", 130, 82);
-            AddLabel("Boyut:", 130, 112);
-            AddLabel("Test Fonksiyonu:", 180, 143);
-        }
-
-        private void AddLabel(string text, int x, int y)
-        {
-            var label = new Label
-            {
-                Text = text,
-                Location = new Point(x, y),
-                AutoSize = true
-            };
-            this.Controls.Add(label);
+            buttonSolve.Click += SolveButton_Click;
         }
 
         private void SolveButton_Click(object sender, EventArgs e)
         {
             try
             {
-                _solveButton.Enabled = false;
+                buttonSolve.Enabled = false;
                 Cursor = Cursors.WaitCursor;
 
-                int colonySize = (int)_colonySizeNumeric.Value;
-                int maxIterations = (int)_maxIterationsNumeric.Value;
-                int limit = (int)_limitNumeric.Value;
-                int dimension = (int)_dimensionNumeric.Value;
+                int colonySize = (int)numericColony.Value;
+                int maxIterations = (int)numericIter.Value;
+                int limit = (int)numericLimit.Value;
+                int dimension = (int)numericDim.Value;
 
                 // Sınırları belirle
                 double[] lowerBound = new double[dimension];
@@ -167,7 +38,7 @@ namespace ABCAlg
 
                 // Test fonksiyonunu seç
                 Func<double[], double> objectiveFunction;
-                switch (_functionComboBox.SelectedItem.ToString())
+                switch (comboFunc.SelectedItem.ToString())
                 {
                     case "Sphere":
                         objectiveFunction = TestFunctions.Sphere;
@@ -188,24 +59,39 @@ namespace ABCAlg
                 _abc.Solve();
 
                 // Sonuçları göster
-                _resultTextBox.Text = $"Test Fonksiyonu: {_functionComboBox.SelectedItem}\r\n";
-                _resultTextBox.Text += $"Kolon Boyutu: {colonySize}\r\n";
-                _resultTextBox.Text += $"Maksimum İterasyon: {maxIterations}\r\n";
-                _resultTextBox.Text += $"Limit: {limit}\r\n";
-                _resultTextBox.Text += $"Boyut: {dimension}\r\n\r\n";
-                _resultTextBox.Text += $"En İyi Amaç Fonksiyonu Değeri: {_abc.BestFitness:F10}\r\n\r\n";
-                _resultTextBox.Text += "En İyi Çözüm:\r\n";
+                textResult.Text = $"Test Fonksiyonu: {comboFunc.SelectedItem}\r\n";
+                textResult.Text += $"Kolon Boyutu: {colonySize}\r\n";
+                textResult.Text += $"Maksimum İterasyon: {maxIterations}\r\n";
+                textResult.Text += $"Limit: {limit}\r\n";
+                textResult.Text += $"Boyut: {dimension}\r\n\r\n";
+                textResult.Text += $"En İyi Amaç Fonksiyonu Değeri: {_abc.BestFitness:F10}\r\n\r\n";
+                textResult.Text += "En İyi Çözüm:\r\n";
                 for (int i = 0; i < _abc.BestSolution.Length; i++)
                 {
-                    _resultTextBox.Text += $"x{i + 1} = {_abc.BestSolution[i]:F10}\r\n";
+                    textResult.Text += $"x{i + 1} = {_abc.BestSolution[i]:F10}\r\n";
                 }
 
                 // Yakınsama grafiğini çiz
-                _convergenceChart.Series[0].Points.Clear();
+                chartConvergence.Series.Clear();
+                if (chartConvergence.ChartAreas.Count == 0)
+                {
+                    chartConvergence.ChartAreas.Add(new ChartArea());
+                }
+                var series = new Series
+                {
+                    ChartType = SeriesChartType.Line,
+                    Color = Color.Blue,
+                    BorderWidth = 2
+                };
+                chartConvergence.Series.Add(series);
                 for (int i = 0; i < _abc.ConvergenceHistory.Count; i++)
                 {
-                    _convergenceChart.Series[0].Points.AddXY(i, _abc.ConvergenceHistory[i]);
+                    series.Points.AddXY(i, _abc.ConvergenceHistory[i]);
                 }
+                chartConvergence.ChartAreas[0].AxisX.Title = "İterasyon";
+                chartConvergence.ChartAreas[0].AxisY.Title = "Amaç Fonksiyonu Değeri";
+                chartConvergence.ChartAreas[0].AxisY.IsLogarithmic = true;
+                chartConvergence.ChartAreas[0].AxisY.LogarithmBase = 10;
             }
             catch (Exception ex)
             {
@@ -213,14 +99,9 @@ namespace ABCAlg
             }
             finally
             {
-                _solveButton.Enabled = true;
+                buttonSolve.Enabled = true;
                 Cursor = Cursors.Default;
             }
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
         }
     }
 }
